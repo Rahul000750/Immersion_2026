6092323246caa7c1d6efcbcbd604af670385bd2d
Kunal Upadhyay <ukunal306@gmail.com>
2026-06-17 08:59:01 +0530

    BankApplication

---
+++ OnlineDeliverySystem/OnlineFood.java
@@ -0,0 +1,21 @@
+package OnlineDeliverySystem;
+
+import java.sql.SQLOutput;
+
+class FoodItem{
+    private int ItemID;
+    private String ItemName;
+    private Double ItemPrice;
+    private int Quantity;
+    public void DisplayFoodItem(){
+        System.out.println("FoodItem");
+        System.out.println("ItemName");
+        System.out.println("ItemPrice");
+        System.out.println("Quantity");
+    }
+}
+public class OnlineFood {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
